8b8a77d94b79c8e2551e2addabe6825a7d1a21cf
jmjmjames <chungjm0711@gmail.com>
2022-09-22 01:01:14 +0900

    [Feat] add RedisConfig (#2)

---
+++ src/main/java/com/example/chattingserver/config/EmbeddedRedisConfig.java
@@ -0,0 +1,28 @@
+package com.example.chattingserver.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.IOException;
+
+@Configuration
+public class EmbeddedRedisConfig {
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void startRedis() throws IOException {
+        redisServer = new RedisServer(redisPort);
+        redisServer.start(); //Redis 시작
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        redisServer.stop(); //Redis 종료
+    }
+}
---
+++ src/main/java/com/example/chattingserver/config/RedisConfig.java
@@ -0,0 +1,33 @@
+package com.example.chattingserver.config;
+
+import com.example.chattingserver.dto.ChatMessage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory connectionFactory) {
+        RedisMessageListenerContainer listenerContainer = new RedisMessageListenerContainer();
+        listenerContainer.setConnectionFactory(connectionFactory);
+        return listenerContainer;
+    }
+
+    @Bean
+    RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer((StandardCharsets.UTF_8)));
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(ChatMessage.class));
+        redisTemplate.setEnableTransactionSupport(true);
+        return redisTemplate;
+    }
+}
