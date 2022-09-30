67daa9d495dca275a3ccad9bf4568d26a178fd27
jmjmjames <chungjm0711@gmail.com>
2022-10-01 00:02:58 +0900

    [Feat] add SecurityConfig (#4)

---
+++ src/main/java/com/example/chattingserver/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.example.chattingserver.config;
+
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
+
+@EnableWebSecurity
+@Configuration
+@ComponentScan(basePackageClasses = {KeycloakSecurityComponents.class})
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+    private static final String ROLE_USER = "user";
+    private static final String ROLE_ADMIN = "admin";
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder managerBuilder) {
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = new KeycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        managerBuilder.authenticationProvider(keycloakAuthenticationProvider);
+    }
+
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/products/**").permitAll()
+                .requestMatchers(new NegatedRequestMatcher(new AntPathRequestMatcher("/chatting/**")))
+                .hasAnyRole(ROLE_USER, ROLE_ADMIN)
+                .anyRequest().permitAll();
+    }
+}
